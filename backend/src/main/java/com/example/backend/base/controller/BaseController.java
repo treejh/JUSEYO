987db10fb344280b8544d45da7d79b016b07ac45
jihyun <jihyun3066@naver.com>
2025-05-08 17:38:24 +0900

    [ fix ] : #4 base 디렉토리 오류 수정

--- backend/src/main/java/com/example/backend/base/controller/BaseController.java
+++ backend/src/main/java/com/example/backend/base/controller/BaseController.java
@@ -1,17 +1,16 @@
 package com.example.backend.base.controller;
 
 
-import com.example.demo.dto.BoardDto;
-import com.example.demo.entity.BoardEntity;
+import com.example.backend.base.dto.request.BoardRequestDto;
+import com.example.backend.base.service.BoardService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
-import com.example.demo.mapper.BoardMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import com.example.demo.service.BoardService;
+
 
 @RestController
 @RequestMapping("/board")
@@ -20,11 +19,10 @@ import com.example.demo.service.BoardService;
 public class BaseController {
 
     private final BoardService BoardService;
-    private final BoardMapper BoardMapper;
 
     // Create
     @PostMapping
-    public ResponseEntity postMember(@Valid @RequestBody BoardDto.Post post) {
+    public ResponseEntity postMember(@Valid @RequestBody BoardRequestDto boardRequestDto) {
         //BoardEntity response = BoardService.createBoard(BoardMapper.boardDtoPostToBoard(post));
 
         // return new ResponseEntity<>(response, HttpStatus.CREATED);
@@ -39,7 +37,7 @@ public class BaseController {
 
     // Update
     @PatchMapping
-    public ResponseEntity patchMember(@RequestBody BoardDto.Patch patch) {
+    public ResponseEntity patchMember(@RequestBody BoardRequestDto patch) {
 
         return new ResponseEntity<>( HttpStatus.OK);
     }
