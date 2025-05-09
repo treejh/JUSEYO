76c2e7f79eaeb9511cd53cee9f073fb686ccb61c
jiyuuuuun <hwangjy001@naver.com>
2025-05-09 13:17:35 +0900

    [back/feat] : #19 예외 코드 수정

--- backend/src/main/java/com/example/backend/exception/ExceptionCode.java
+++ backend/src/main/java/com/example/backend/exception/ExceptionCode.java
@@ -3,9 +3,9 @@ package com.example.backend.exception;
 import lombok.Getter;
 
 public enum ExceptionCode {
-     BLOG_NOT_FOUND(404, "블로그를 찾을 수 없습니다."),
+    MAIN_DASHBOARD_NOT_FOUND(404,"존재하지 않는 메인 페이지 입니다"),
     S3_DELETE_ERROR(404, "이미지를 삭제할 수 없습니다."),
-     USER_NOT_FOUND(404,"유저를 찾을 수 없습니다. "),
+    USER_NOT_FOUND(404,"유저를 찾을 수 없습니다. "),
     BOARD_NOT_FOUND(404,"게시판을 찾을 수 없습니다. "), //삭제 해도 됨
     ALREADY_HAS_BLOG(404, "블로그를 이미 가지고 있습니다."),
     ROLE_NOT_FOUND(404,"존재하지 않는 역할입니다."),
