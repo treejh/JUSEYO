9ce4c241eb26c0b6263ffa7ec885fc06e8442dbd
jiyuuuuun <hwangjy001@naver.com>
2025-05-09 17:34:05 +0900

    [back/feat]27:중복 클래스 제거

--- backend/src/main/java/com/example/backend/managementDashboard/repository/ManagementDashboardRepository.java
+++ backend/src/main/java/com/example/backend/managementDashboard/repository/ManagementDashboardRepository.java
@@ -3,5 +3,8 @@ package com.example.backend.managementDashboard.repository;
 import com.example.backend.managementDashboard.entity.ManagementDashboard;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ManagementDashboardRepository extends CrudRepository<ManagementDashboard, Long> {
+    Optional<ManagementDashboard> findByName(String name);
 }
