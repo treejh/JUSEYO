f2278074af73d19f03cbff0afa7041132463ca94
jiyuuuuun <hwangjy001@naver.com>
2025-05-12 11:43:01 +0900

    [back/feat]34:StringToInboundConverter 클래스 생성

---
+++ backend/src/main/java/com/example/backend/converter/StringToInboundConverter.java
@@ -0,0 +1,18 @@
+package com.example.backend.converter;
+
+import com.example.backend.enums.Inbound;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StringToInboundConverter implements Converter<String, Inbound> {
+
+    @Override
+    public Inbound convert(String source) {
+        try {
+            return Inbound.valueOf(source.trim().toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid status value: " + source);
+        }
+    }
+}
