4bce75353d5dea5a1db2e052adcfc6090b86ac2c
jihyun <jihyun3066@naver.com>
2025-05-14 13:27:51 +0900

    [ back / feat ] 85 : 이미지 생성, 수정, 삭제 구현 완료

--- backend/src/main/java/com/example/backend/config/AwsS3Config.java
+++ backend/src/main/java/com/example/backend/config/AwsS3Config.java
@@ -1,6 +1,11 @@
 package com.example.backend.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
