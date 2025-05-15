2e6c805b55876be2dab345a8fd50ff631afb8460
jiyuuuuun <hwangjy001@naver.com>
2025-05-15 10:26:37 +0900

    [back/feat]84:권한 설정

--- backend/src/main/java/com/example/backend/security/config/SecurityConfigJuseyo.java
+++ backend/src/main/java/com/example/backend/security/config/SecurityConfigJuseyo.java
@@ -55,6 +55,14 @@ public class SecurityConfigJuseyo {
                         // 알림(테스트 목적으로 permitAll)
                         .requestMatchers(HttpMethod.POST, "/api/v1/notifications/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/notifications/stream/**").permitAll()
+                        //비품 등룩(구매)
+                        .requestMatchers("/api/v1/register-items/**").hasAnyRole("MANAGER", "ADMIN")
+                        //비품 반납
+                        .requestMatchers("/api/v1/supply-return/**").hasAnyRole("MANAGER", "ADMIN","USER")
+                        .requestMatchers(HttpMethod.POST,"/api/v1/supply-return/{id}").hasAnyRole("MANAGER", "ADMIN") //반납서 상태 변경
+                        //입고
+                        .requestMatchers("/api/v1/inventory-in/**").hasAnyRole("MANAGER", "ADMIN")
+
 
                         .anyRequest().authenticated()
                 )
