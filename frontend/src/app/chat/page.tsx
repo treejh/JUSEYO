34300c5c2b83f9c91b70f975d2b4a0b4b7b36609
jihyun <jihyun3066@naver.com>
2025-05-15 09:29:18 +0900

    [ back / front ] 75 : 쿠키에 담아서 보냄 ?

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -12,23 +12,19 @@ const ChatPage = () => {
   const username = "jihyun1"; // 사용자 이름
 
   useEffect(() => {
-    // // 쿠키에서 accessToken 추출
-    // const getAccessTokenFromCookie = () => {
-    //   const cookies = document.cookie.split(";").map((cookie) => cookie.trim());
-    //   const accessTokenCookie = cookies.find((cookie) =>
-    //     cookie.startsWith("accessToken=")
-    //   );
-    //   return accessTokenCookie ? accessTokenCookie.split("=")[1] : null;
-    // };
-
-    // const accessToken = getAccessTokenFromCookie();
+    function getCookie(name: string): string | null {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop()?.split(";").shift() || null;
+      return null;
+    }
 
     const stompClient = new Client({
       webSocketFactory: () => new SockJS("http://localhost:8080/ws-stomp"),
       debug: (str) => console.log(str),
-      //   connectHeaders: {
-      //     Authorization: `Bearer ${accessToken}`, // 헤더에 accessToken 추가
-      //   },
+      connectHeaders: {
+        accessToken: getCookie("accessToken") || "",
+      },
       onConnect: () => {
         console.log("WebSocket 연결 성공");
 
