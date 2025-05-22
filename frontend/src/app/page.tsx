bf43e79cb691b6a6bcd668ec5296a92e3a6a1ac0
hs.o_k <hs.o_k@naver.com>
2025-05-22 14:37:54 +0900

    [ front / style ] 174 : 비품요청관리 풀 받기전

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -376,8 +376,8 @@ export default function Home() {
                   <button
                     onClick={() =>
                       !loginUser.managementDashboardName
-                        ? router.push('/admin/request')
-                        : router.push('/dashboard')
+                        ? router.push("/admin/request")
+                        : router.push("/dashboard")
                     }
                     className="inline-flex items-center px-6 py-3 bg-gradient-to-r from-blue-600 to-indigo-600 text-white text-sm font-medium rounded-lg shadow-md hover:shadow-lg transform hover:-translate-y-0.5 transition-all duration-200 group"
                   >
@@ -395,7 +395,7 @@ export default function Home() {
                         d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
                       />
                     </svg>
-                    {!loginUser.managementDashboardName 
+                    {!loginUser.managementDashboardName
                       ? "관리자 페이지 생성"
                       : "관리자 페이지 접속"}
                     <svg
