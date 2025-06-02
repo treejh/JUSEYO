792cb7f55104260d4edb41dee84ac344d6a7f297
jiyuuuuun <hwangjy001@naver.com>
2025-06-02 21:37:02 +0900

    [ front / refact ] 514 : 일반 대시보드 수정

---
+++ backend/src/main/java/com/example/backend/domain/supply/controller/SupplyController.java
@@ -0,0 +1,9 @@
+package com.example.backend.domain.supply.service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SupplyController {
+}
