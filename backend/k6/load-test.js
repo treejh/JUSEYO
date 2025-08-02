3bf78fe925b79a95aae4db9c6e5ecd41df9c4fd2
jihyun <jihyun3066@naver.com>
2025-08-03 01:13:58 +0900

    [ feat ] 519 : 부하 테스트 1000 vue로 수정

--- backend/k6/load-test.js
+++ backend/k6/load-test.js
@@ -2,8 +2,13 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-    vus: 100,
-    duration: '10s',
+    stages: [
+        { duration: '10s', target: 100 },
+        { duration: '10s', target: 300 },
+        { duration: '10s', target: 500 },
+        { duration: '10s', target: 1000 },
+        { duration: '10s', target: 0 }, // 종료
+    ],
 };
 
 const BASE_URL = 'http://host.docker.internal:8080';
