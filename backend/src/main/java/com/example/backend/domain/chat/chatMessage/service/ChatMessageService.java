89d04e988f9f31c32ab1f7ad6997b7fd93330a1e
jihyun <jihyun3066@naver.com>
2025-08-03 01:38:37 +0900

    [ feat ] 519 : 사용하지 않는 메서드 삭제

--- backend/src/main/java/com/example/backend/domain/chat/chatMessage/service/ChatMessageService.java
+++ backend/src/main/java/com/example/backend/domain/chat/chatMessage/service/ChatMessageService.java
@@ -153,34 +153,6 @@ public class ChatMessageService {
         return chatMessageRepository.save(chatMessage);
     }
 
-//    @RedisCacheLock(key = "'chatroom:' + #roomId + ':messages:page:' + #pageable.pageNumber")
-//    public Page<ChatResponseDto> getChatMessage(Long roomId, Pageable pageable) {
-//        User user = userService.findById(tokenService.getIdFromToken());
-//        ChatRoom chatRoom = chatRoomService.findChatRoomById(roomId);
-//
-//        if (chatUserRepository.findByUserAndChatRoom(user, chatRoom).isEmpty()) {
-//            throw new BusinessLogicException(ExceptionCode.NOT_ENTER_CHAT_ROOM);
-//        }
-//
-//        List<ChatResponseDto> cached = chatMessageRedisService.getCachedMessages(roomId,pageable.getPageNumber());
-//
-//        // 락을 잡은 뒤에도 누군가 캐시를 넣었을 수 있으니 다시 확인 (더블 체크)
-//        if (cached != null && !cached.isEmpty()) {
-//            //캐시 HIT일 경우: 가져온 메시지 리스트를 바로 응답
-//            //PageImpl은 Spring Data의 페이지 응답 객체
-//            return new PageImpl<>(cached, pageable, cached.size());
-//        }
-//
-//        //아니면 DB에서 조회
-//        Page<ChatMessage> chatMessagePage = chatMessageRepository.findByChatRoom(chatRoom, pageable);
-//        List<ChatResponseDto> result = chatMessagePage.map(ChatResponseDto::new).toList();
-//
-//        //redis에 저장
-//        chatMessageRedisService.cacheMessages(roomId,result,Duration.ofSeconds(chatMessageRedisService.messageTTL),pageable.getPageNumber());
-//
-//        return new PageImpl<>(result, pageable, result.size());
-//    }
-
     public Page<ChatResponseDto> getChatMessage(Long roomId, Pageable pageable) {
 
         List<ChatResponseDto> cached = chatMessageRedisService.getCachedMessages(roomId, pageable.getPageNumber());
