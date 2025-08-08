a3b8aabbfcf581490ffa9b867ca6663e1a0f8eef
jihyun <jihyun3066@naver.com>
2025-08-08 15:25:37 +0900

    [ feat ] 524 : rabbitMQ 도커 추가

--- backend/k6/load-test.js
+++ backend/k6/load-test.js
@@ -4,10 +4,10 @@ import { check, sleep } from 'k6';
 export const options = {
     stages: [
         { duration: '10s', target: 100 },
-        { duration: '10s', target: 300 },
-        { duration: '10s', target: 500 },
-        { duration: '10s', target: 1000 },
-        { duration: '10s', target: 0 }, // 종료
+        // { duration: '10s', target: 300 },
+        // { duration: '10s', target: 500 },
+        // { duration: '10s', target: 1000 },
+        // { duration: '10s', target: 0 }, // 종료
     ],
 };
 
